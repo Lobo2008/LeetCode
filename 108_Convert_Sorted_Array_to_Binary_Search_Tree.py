cb2fa089c40f397484ea3ea54b5ccc5bd69682d3
luozonghai <842315999@qq.com>
2018-05-30 16:11:07 +0800

    tree construct

---
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,37 @@
+"""
+Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+Example:
+
+Given the sorted array: [-10,-3,0,5,9],
+
+One possible answer is: [0,-3,9,-10,null,5], which represents the following height balanced BST:
+
+      0
+     / \
+   -3   9
+   /   /
+ -10  5
+"""
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        """
+        给定一个升序的list，返回一棵二叉搜索树，
+        二叉搜索树的定义：
+            它或者是一棵空树，或者是具有下列性质的二叉树： 若它的左子树不空，则左子树上所有结点的值均小于它的根结点的值； 若它的右子树不空，则右子树上所有结点的值均大于它的根结点的值； 它的左、右子树也分别为二叉排序树。
+        简单的说，就是满足：左 < 根 < 右
+        """
