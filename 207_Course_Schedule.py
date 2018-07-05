56d73915be3c768a812d2f03446930185dc4ee45
luozonghai <842315999@qq.com>
2018-07-05 23:31:43 +0800

    BFS

--- 207_Course_Schedule.py
+++ 207_Course_Schedule.py
@@ -45,31 +45,152 @@ class Solution:
 
 
         """
+        if len(prerequisites) == 0:
+            return True
+        #初始化每个节点的入度为0
+        in_degrees = {}
+        for u,v in prerequisites:
+            if u not in in_degrees:
+                in_degrees[u] = 0
+            if v not in prerequisites:
+                in_degrees[v] = 0
+        #计算每个节点的入度
+        for u,v in prerequisites:
+            in_degrees[v] += 1
+        print(in_degrees)
+        Q = []
+        Seq = []
+        #找一个入度为0的节点作为开始，将节点加入序列Seq
+        for item in in_degrees:
+            if in_degrees[item] == 0:
+                Q.append(item)
+                in_degrees.pop(item)
+                break
+        while Q:            
+            u = Q.pop()
+            Seq.append(u)
+            for ux,vx in prerequisites:
+                if u == ux:
+                    in_degrees[vx] -= 1
+            for k,v in in_degrees.items():
+                if v == 0:
+                    Q.append(k)
+                    in_degrees.pop(k)
+                    break
+        if len(Seq) == numCourses:
+            return True
+        return False
+                
 
-        pre_post_map ={} # key is one course, value is a set of all courses need this course as prerequisite
-        post_pre_map = {} # key is one course, value is a set of all courses need to learn before learning this course
-        for post, pre in prerequisites:#post是key，pre是val，即p=[[高数下,高数下],[大三,大2]]的话，第一次：高数上->高数下,第二次：大3->大2
-            if post not in post_pre_map:
-                post_pre_map[post] = set()
-            post_pre_map[post].add(pre)#po={"高数下":”高数上“},要选高数下，必须先选高数上
-            if pre not in pre_post_map:#
-                pre_post_map[pre] = set()
-            pre_post_map[pre].add(post) #po={”高数上“：”高数下“}，选了高数上以后，可以选高数下           
-        
-        """
-        set({"lzh":1992})={"lzh"}取的是key
 
-        set({大1，大2，大3，大4}) ^ set({大4，大2})  =>  {大1，大3}
-        """
+so = Solution()
 
-        next_to_learn = set(range(numCourses)) ^ set(post_pre_map) # learn the ones that do not have any prerequiste course first (base courses)
-        learned_courses = set() # courses already learned so far
-        while next_to_learn: # still have courses to learn
-            course = next_to_learn.pop() # learn this course
-            learned_courses.add(course) # this course now learned
-            if course in pre_post_map: # if this course is a prerequiste for other course(s)
-                for post_course in pre_post_map[course]:
-                    post_pre_map[post_course].remove(course) # this prerequiste course for this post_course is learned
-                    if post_pre_map[post_course] == set(): # all prerequiste course(s) for this post_course are learned
-                        next_to_learn.add(post_course) # now we can learn this higher level course
-        return len(learned_courses) == numCourses
+numCourses = 2
+prerequisites =  [[1,0],[0,1]]
+
+numCourses = 4
+prerequisites = [[4,3],[4,2],[1,2],[1,3],[2,1]]
+# prerequisites = [[4,3],[4,2],[1,2],[1,3]]
+print(so.canFinish(numCourses, prerequisites))
+
+# """
+# ref：https://www.cnblogs.com/zhaojieyu/p/8543136.html
+# 1）先计算所有节点的入度
+# 2）然后将入度为0的加入队列，并将其从图里移去，再将移去的元素指向的元素的入读减一，入度为0的就是没有前提要求的项目
+# 3）重复以上步骤，如果最终队列的长度等于图的边的数量，说明无环
+# """
+
+# def toposort(graph):
+#     """
+#     {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0}
+#     """
+#     in_degrees = dict((u,0) for u in graph)   #初始化所有顶点入度为0
+#     vertex_num = len(in_degrees)#边的数量
+#     """
+#     G = {
+#         'a':'bce',
+#         'b':'d',
+#         'c':'bd',
+#         'd':'',
+#         'e':'cd'
+#     }
+#     1)  u=a
+#             v=b
+#                  {'a': 0, 'b': 1, 'c': 0, 'd': 0, 'e': 0}
+#             v=c
+#                  {'a': 0, 'b': 1, 'c': 1, 'd': 0, 'e': 0}
+#             v=e
+#                  {'a': 0, 'b': 1, 'c': 1, 'd': 0, 'e': 1}
+#     2) u=b
+#             v=d
+#                 {'a': 0, 'b': 1, 'c': 1, 'd': 1, 'e': 1}
+#     3) u=c
+#             v=b
+#                  {'a': 0, 'b': 2, 'c': 1, 'd': 1, 'e': 1}
+#             v=d
+#                {'a': 0, 'b': 2, 'c': 1, 'd': 2, 'e': 1}
+#     4) u=d
+#             no changes
+#     5) u=e
+#             v=c
+#                  {'a': 0, 'b': 2, 'c': 2, 'd': 2, 'e': 1}
+#             v=d
+#                  {'a': 0, 'b': 2, 'c': 2, 'd': 3, 'e': 1}
+#     """
+#     for u in graph:
+#         for v in graph[u]:
+#             in_degrees[v] += 1       #计算每个顶点的入度
+#     print(in_degrees)
+#     Q = [u for u in in_degrees if in_degrees[u] == 0]   # 筛选入度为0的顶点
+    
+#     Seq = []
+#     """
+#     1) Q=[a],Seq=[] 
+#        u=a,Seq=[a]
+#        v=b
+#             {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 1},   b!=0
+#        v=c
+#             {'a': 0, 'b': 1, 'c': 1, 'd': 3, 'e': 1}    c!=0
+#        v=e 
+#             {'a': 0, 'b': 1, 'c': 1, 'd': 3, 'e': 0}    e=0 -> Q=[e]
+#     2） Q=[e],Seq=[a]
+#         u=[e],Seq=[a,e]
+#         v=c
+#            {'a': 0, 'b': 1, 'c': 0, 'd': 3, 'e': 0}     c=0 -> Q=[c]
+#         v=d
+#             {'a': 0, 'b': 1, 'c': 0, 'd': 2, 'e': 0} d!=0
+#     3) Q=[c],Seq=[a,e]
+#         u=c,Seq=[a,e,c]
+#         v=b
+#             {'a': 0, 'b': 0, 'c': 0, 'd': 2, 'e': 0}   b=0-> Q=[b]
+#         v=d
+#             {'a': 0, 'b': 0, 'c': 0, 'd': 1, 'e': 0}   d!=0
+#     4)Q=[b],Seq=[a,e,c]
+#         u=d,Seq=[a,e,c,b]
+#              {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0} d=0 -> Q=[d]
+#     5)Q=[d],Seq=[a,e,c,d]
+#         u='',Seq=[a,e,c,b,d]
+
+
+#     """
+
+#     while Q:
+#         u = Q.pop()       #默认从最后一个删除
+#         Seq.append(u)
+#         for v in graph[u]:
+#             in_degrees[v] -= 1       #移除其所有指向
+#             if in_degrees[v] == 0:
+#                 Q.append(v)          #再次筛选入度为0的顶点
+#         print('u=',u,'->',in_degrees)
+#     if len(Seq) == vertex_num:       #如果循环结束后存在非0入度的顶点说明图中有环，不存在拓扑排序
+#         return Seq
+#     else:
+#         print("there's a circle.")
+# G = {
+#     'a':'bce',
+#     'b':'d',
+#     'c':'bd',
+#     'd':'',
+#     'e':'cd'
+# }
+#  print(toposort(G))
